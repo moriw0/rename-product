8d1427e45a9b2d7a45aa5edab4c3e827e3b7c378
shota <tontonka2@gmail.com>
2022-03-06 17:18:35 +0900

    change interbal

--- cron.js
+++ cron.js
@@ -2,7 +2,6 @@ import cron from 'node-cron';
 import { renameFunc } from './gql.js';
 
 console.log('start cron-job')
-// cron.schedule('*/5 * * * * *', () => renameFunc());
-cron.schedule('* * * * *', () => renameFunc());
+cron.schedule('* * * *', () => renameFunc());
 
 
